0402a892603d0e87cb5417d0553509a893d05630
Benjamin-van-Heerden <benjaminvh1997@gmail.com>
2022-12-11 23:07:49 +0200

    added navbar.

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
+import Script from "next/script";
+import { ThemeProvider } from "next-themes";
+
 import "../styles/globals.css";
+import { Navbar, Footer } from "../components";
 
-const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />;
-
+const MyApp = ({ Component, pageProps }) => (
+  <ThemeProvider attribute="class">
+    <div className="dark:bg-nft-dark bg-white min-h-screen">
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+    <Script src="https://kit.fontawesome.com/e694961baa.js" crossorigin="anonymous" />
+  </ThemeProvider>
+);
 export default MyApp;
